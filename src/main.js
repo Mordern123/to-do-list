f72fac8e91343f5e23cdaabbc1764b1712f35716
Mordern123 <eeric55tw@gmail.com>
2021-12-03 14:14:27 +0800

    html $ css done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 import router from './router'
 import store from './store'
+import Todolist from './Todolist.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(Todolist).use(store).use(router).mount('#app')
