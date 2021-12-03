25c1e3d20ca9366f2b0739568b307366c44d6a66
Mordern123 <eeric55tw@gmail.com>
2021-12-04 00:40:57 +0800

    CR"U"D

--- src/Todolist.vue
+++ src/Todolist.vue
@@ -1,33 +1,82 @@
 <template>
-<section class="container">
-  <div class="heading">
-    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-book" viewBox="0 0 24 16">
-        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-    </svg>
-    <h1 class="heading__title">To-Do List</h1>
-  </div>
-  <form class="form">
-    <div>
-      <label class="form__label" for="todo">~ my fxxking life ~</label>
-      <input class="form__input"
-           placeholder="ready to do~"
-           type="text"
-           v-model="msg"
-           size="30"
-           required>
-      <button class="button" @click="createSubmit"><span>Submit</span></button>
+  <section class="container">
+    <div class="heading">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="70"
+        height="70"
+        fill="currentColor"
+        class="bi bi-book"
+        viewBox="0 0 24 16"
+      >
+        <path
+          d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
+        />
+      </svg>
+      <h1 class="heading__title">To-Do List</h1>
     </div>
-  </form>
-  <div>
-    <ul class="toDoList">
-      <li v-for="(item, index) in todos" :key="item.id">
-        <button class="content" @click="deleteSubmit(item, index)">
-          {{ item.content }}
+    <form class="form">
+      <div>
+        <label class="form__label" for="todo">~ my fxxking life ~</label>
+        <input
+          class="form__input"
+          placeholder="ready to do~"
+          type="text"
+          v-model="msg"
+          size="30"
+          required
+        />
+        <button class="button" @click="createSubmit">
+          <span>Submit</span>
         </button>
-      </li>
-    </ul>
-  </div>
-</section>
+      </div>
+    </form>
+    <div>
+      <ul class="toDoList">
+        <li v-for="(item, index) in todos" :key="item.id">
+          <button type="button" class="complete" @click="updateSubmit(item)">
+            <svg
+              v-if="todos[index].complete"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-check2-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"
+              />
+              <path
+                d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"
+              />
+            </svg>
+            <svg
+              v-else
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+            </svg>
+          </button>
+
+          <button
+            type="button"
+            class="content"
+            @click="deleteSubmit(item, index)"
+          >
+            {{ item.content }}
+          </button>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -40,27 +89,32 @@ export default {
   },
   methods: {
     createSubmit () {
-      const msg = {
-        id: this.todos.length + 1,
-        content: this.msg,
-        complete: false
+      if (this.msg !== '') {
+        const msg = {
+          id: this.todos.length + 1,
+          content: this.msg,
+          complete: false
+        }
+        this.todos.push(msg)
+        this.msg = ''
       }
-      this.todos.push(msg)
-      this.msg = ''
     },
     deleteSubmit (item, index) {
       const result = confirm(`Delete fxxking life:${item.content}?`)
       if (result) {
         this.todos.splice(index, 1)
       }
+    },
+    updateSubmit (item) {
+      item.complete = !item.complete
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Gochi+Hand');
-@import url('https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Gochi+Hand");
+@import url("https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&display=swap");
 
 body {
   background-color: #003366;
@@ -71,7 +125,7 @@ body {
   justify-content: center;
   align-items: center;
   color: #999933;
-  font-family: 'Gochi Hand', cursive;
+  font-family: "Gochi Hand", cursive;
   text-align: center;
   font-size: 130%;
 }
@@ -103,7 +157,7 @@ body {
   font-size: 1.5rem;
 }
 @media only screen and (min-width: 500px) {
-  .heading__title{
+  .heading__title {
     font-size: 2.5rem;
   }
 }
@@ -115,24 +169,24 @@ body {
   margin-bottom: 0.5rem;
 }
 .form__input {
-    box-sizing: border-box;
-    background-color: transparent;
-    padding: 0.7rem;
-    border-bottom-right-radius: 15px 3px;
-    border-bottom-left-radius:3px 15px;
-    border: solid 3px transparent;
-    border-bottom: dashed 3px #999933;
-    font-family: 'Fuzzy Bubbles', cursive;
-    font-size: 1rem;
-    color: hsla(260, 2%, 25%, 0.7);
-    width: 70%;
-    margin-bottom: 20px;
+  box-sizing: border-box;
+  background-color: transparent;
+  padding: 0.7rem;
+  border-bottom-right-radius: 15px 3px;
+  border-bottom-left-radius: 3px 15px;
+  border: solid 3px transparent;
+  border-bottom: dashed 3px #999933;
+  font-family: "Fuzzy Bubbles", cursive;
+  font-size: 1rem;
+  color: hsla(260, 2%, 25%, 0.7);
+  width: 70%;
+  margin-bottom: 20px;
   &:focus {
     border: solid 3px #999933;
   }
 }
 @media only screen and (min-width: 500px) {
- .form__input {
+  .form__input {
     width: 60%;
   }
 }
@@ -142,7 +196,7 @@ body {
   border: none;
   transform: rotate(6deg);
   transform-origin: center;
-  font-family: 'Gochi Hand', cursive;
+  font-family: "Gochi Hand", cursive;
   text-decoration: none;
   padding-bottom: 3px;
   border-radius: 5px;
@@ -157,19 +211,29 @@ body {
   border-radius: 5px;
   border: 2px solid hsl(198, 1%, 29%);
 }
-.button:active, .button:focus {
+.button:active,
+.button:focus {
   transform: translateY(4px);
   padding-bottom: 0px;
   outline: 0;
 }
 .toDoList {
   text-align: left;
-  .content{
-    font-family: 'Fuzzy Bubbles', cursive;
+  .complete {
+    border: none;
+    background-color: transparent;
+    color: #999933;
+  }
+  .complete:hover {
+    color: #336699;
+  }
+  .content {
+    font-family: "Fuzzy Bubbles", cursive;
     position: relative;
     padding: 0.5rem;
-    border:none;
+    border: none;
     background-color: transparent;
+    color: #999933;
   }
   .content:hover {
     text-decoration: line-through #336699;
